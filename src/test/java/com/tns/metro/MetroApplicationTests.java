905c2ec2ecea6d944e60b7f418d3dcd360ffd47d
fzdog <cluster56460>
2018-12-20 13:46:01 -0500

    init project

---
+++ src/test/java/com/tns/metro/MetroApplicationTests.java
@@ -0,0 +1,17 @@
+package com.tns.metro;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MetroApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
